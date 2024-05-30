e854e98f80721fc41e607d6e3e6c828f8f68280c
Slashlolka <s.toktarov98@gmail.com>
2024-05-31 00:03:47 +0300

    HW #6

--- Assets/Scripts/UnitBrains/Player/SecondUnitBrain.cs
+++ Assets/Scripts/UnitBrains/Player/SecondUnitBrain.cs
@@ -1,6 +1,9 @@
 ﻿using System.Collections.Generic;
+using Model;
 using Model.Runtime.Projectiles;
 using UnityEngine;
+using Utilities;
+using static UnityEngine.GraphicsBuffer;
 
 namespace UnitBrains.Player
 {
@@ -12,7 +15,8 @@ namespace UnitBrains.Player
         private float _temperature = 0f;
         private float _cooldownTime = 0f;
         private bool _overheated;
-        
+        private List<Vector2Int> _targetsOutOfReach = new List<Vector2Int>();
+
         protected override void GenerateProjectiles(Vector2Int forTarget, List<BaseProjectile> intoList)
         {
             float overheatTemperature = OverheatTemperature;
@@ -26,7 +30,8 @@ namespace UnitBrains.Player
 
         public override Vector2Int GetNextStep()
         {
-            return base.GetNextStep();
+            if (_targetsOutOfReach.Count <= 0 || IsTargetInRange(_targetsOutOfReach[0])) return unit.Pos;
+            return unit.Pos.CalcNextStepTowards(_targetsOutOfReach[0]);
         }
 
         protected override List<Vector2Int> SelectTargets()
@@ -34,10 +39,34 @@ namespace UnitBrains.Player
             ///////////////////////////////////////
             // Homework 1.4 (1st block, 4rd module)
             ///////////////////////////////////////
-            List<Vector2Int> result = GetReachableTargets();
-            while (result.Count > 1)
+
+            List<Vector2Int> result = new List<Vector2Int>(GetAllTargets());
+            _targetsOutOfReach.Clear();
+            if (result.Count > 0)
             {
-                result.RemoveAt(result.Count - 1);
+                Vector2Int outTarget = new();
+                float minDist = float.MaxValue;
+                float nowDist = 0;
+
+                foreach (Vector2Int nowTarget in result)
+                {
+                    nowDist = DistanceToOwnBase(nowTarget);
+                    if(nowDist < minDist)
+                    {
+                        minDist = nowDist;
+                        outTarget = nowTarget;
+                    }
+                }
+                _targetsOutOfReach.Add(outTarget);
+
+                result.Clear();
+                if (IsTargetInRange(outTarget)) result.Add(outTarget);
+                else _targetsOutOfReach.Add(outTarget);
+            }
+            else
+            {
+                result.Add(runtimeModel.RoMap.Bases[IsPlayerUnitBrain ? RuntimeModel.BotPlayerId : RuntimeModel.PlayerId]);
+                _targetsOutOfReach.Add(runtimeModel.RoMap.Bases[IsPlayerUnitBrain ? RuntimeModel.BotPlayerId : RuntimeModel.PlayerId]);
             }
             return result;
             ///////////////////////////////////////
